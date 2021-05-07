cb9de3a603a937b68296c0aef762215877a2fc31
wenliangliang <363237987@qq.com>
2021-05-07 15:50:42 +0800

    first commit

---
+++ MrPotato/Assets/Scripts/CameraFallow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour
+{
+    Transform playerTran;
+    float MaxDistanceX = 2;
+    float MaxDistanceY = 2;
+    float xSpeed = 4;
+    float ySpeed = 4;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Awake()
+    {
+        playerTran = transform.Find("Player");
+    }
+    private bool NeedMoveX()
+    {
+        bool bMove = false;
+        if (Mathf.Abs(transform.position.x = playerTran.position.x))
+        {
+            bMove = true;
+        }
+        else
+            bMove = false;
+        return bMove;
+    }
+}
---
+++ MrPotato/Assets/Scripts/PlayerContro.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContro : MonoBehaviour
+{
+    float horizontal;
+    float MaxSpeed = 5f;
+    bool facingRight = true;
+    bool bGroundCheck = false;
+    float jumpForce = 500f;
+    float moveForce = 100f;
+    Rigidbody2D rigidbody;
+    Transform mGroundCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        mGroundCheck = transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    //移动
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontal * moveForce);
+       
+        if (horizontal * GetComponent<Rigidbody2D>().velocity.x < MaxSpeed)
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontal * moveForce);
+        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > MaxSpeed)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * MaxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        
+        if (horizontal > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if (horizontal < 0 && facingRight)
+        {
+            flip();
+        }
+       
+        
+        Physics2D.Linecast(transform.position, mGroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        bool mJump = false;
+        
+        if (mGroundCheck)
+        {
+            mJump = Input.GetKeyDown(KeyCode.Space);
+            if (mJump)
+            {
+                Vector2 upForce = new Vector2(0f, jumpForce);
+
+                rigidbody.AddForce(upForce);
+                mJump = false;
+            }
+            
+           
+        }
+        
+
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
